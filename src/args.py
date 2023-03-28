6b5449a344b8df8f2887f5ac732175c4b194041c
William Simpson <101291178+williamdoesdev@users.noreply.github.com>
2023-03-28 10:35:37 -0500

    Update args.py

--- src/args.py
+++ src/args.py
@@ -1,6 +1,6 @@
 import argparse
 
-def parse_arguments():
+def parse_arguments(args_list=None):
     parser = argparse.ArgumentParser(description="CLI interface for the chatbot")
 
     parser.add_argument('--question', '-q', type=str, help="Ask a question to the chatbot")
@@ -9,5 +9,8 @@ def parse_arguments():
     parser.add_argument('--delete-chat', '-d', type=str, help="Delete a chat by ID")
     parser.add_argument('--list-chats', '-ls', action='store_true', help="List all chats")
 
-    args = parser.parse_args()
+    if args_list:  # If args_list is provided, pass it to parse_args
+        args = parser.parse_args(args_list)
+    else:
+        args = parser.parse_args()
     return args
